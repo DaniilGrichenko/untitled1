cd05e14a22e172e27abcc376dff2e95439c4771f
Daniil Grichenko <palka-fc@ukr.net>
2022-04-29 21:25:45 +0300

    added files

---
+++ src/lab_56.java
@@ -0,0 +1,19 @@
+public class lab_56 {
+    public static void main(String[] arg) {
+        int a = 90;
+        int b = 25  ;
+        int d = a / b;
+        int c = a % b;
+        System.out.println("1)" +d );
+        System.out.println("1)" +c );
+        //------------------//
+        int h = 74;
+        int p=h%10;
+        int r=(h/10)%10;
+        System.out.println("2)" + h + "=" + (p+r));
+        //------------------//
+        double x = 8.5;
+        System.out.println("3)"+Math.rint(x));
+        //-----------------//
+    }
+}
